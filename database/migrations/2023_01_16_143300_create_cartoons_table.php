9c2b245739dcd99833a561f5378fe11222e4871c
Debora Cocchi <108935821+DeboraCocchi@users.noreply.github.com>
2023-01-16 15:39:06 +0100

    [add]model-e-migration

---
+++ database/migrations/2023_01_16_143300_create_cartoons_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cartoons', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',100);
+            $table->string('slug',150)->unique();
+            $table->year('year');
+            $table->string('creator');
+            $table->string('rating',20);
+            $table->string('genre');
+            $table->tinyInteger('runtime_in_minutes');
+            $table->smallInteger('episodes');
+            $table->string('image');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cartoons');
+    }
+};
