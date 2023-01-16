47ab803f51f4db1f72ccc8f2ff95e9cb9c40b61a
andreaa21 <andrea.zollini@proton.me>
2023-01-16 16:12:25 +0100

    [add] seeder

--- app/Models/Cartoon.php
+++ app/Models/Cartoon.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Cartoon extends Model
 {
     use HasFactory;
+
+    public static function generateSlug($string)
+    {
+        $slug = Str::slug($string, '-');
+        $original_slug = $slug;
+        $c = 1;
+        $cartoon_exists = Cartoon::where('slug', $slug)->first();
+
+        while ($cartoon_exists) {
+            $slug = $original_slug . '-' . $c;
+            $cartoon_exists = Cartoon::where('slug', $slug)->first();
+            $c++;
+        }
+        return $slug;
+    }
 }
